a1fdf4805e8a614031ee3aa0380ef8639cb114f4
Martin Fondovila <fondovila.martin@gmail.com>
2022-08-08 20:31:46 -0300

    Feat: Inicialización del proyecto.

---
+++ src/scenes/BootLoader.js
@@ -0,0 +1,13 @@
+class BootLoader extends Phaser.Scene {
+  constructor() {
+    super("BootLoaderScene");
+  }
+  init() {
+    console.log("Scene Bootloader");
+  }
+  preload() {}
+  create() {}
+  update(time, delta) {}
+}
+
+export default BootLoader;
