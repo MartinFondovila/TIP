bf8c61432e477924e72c23c9e4472d9f08561b7f
Martin Fondovila <fondovila.martin@gmail.com>
2022-09-05 18:36:24 -0300

    feat: Se agrego hitbox para los ataques de los personajesy tambien se implemento la logica basica de la pelea, es decir, se pueden golpear y cuando su vida llega a 0 ya no pueden hacer nada.

--- src/classes/ninja.js
+++ src/classes/ninja.js
@@ -2,43 +2,35 @@ import { Fighter } from "../classes/fighter.js";
 
 export class Ninja extends Fighter {
   constructor(scene, x, y, controls) {
-    super(scene, x, y, controls, "minotaur", 120, 10);
+    super(scene, x, y, controls, "minotaur", 30, 10);
     this.body.setSize(30, 40);
     this.body.setOffset(33.5, 24);
     this.setScale(1.5);
+
+    this.baseDamage = 10;
+
+    this.attackHitbox = scene.add.rectangle(0, 0, 32, 64, 0xffffff, 0.5);
+    scene.physics.add.existing(this.attackHitbox);
+    this.attackHitbox.body.setAllowGravity(false);
   }
 
-  attack() {
-    super.attack("attack");
+  setAnimationsOnMap() {
+    this.animationsMap.set("idle", "idle");
+    this.animationsMap.set("walk", "walking");
+    this.animationsMap.set("jump", "jump");
+    this.animationsMap.set("fall", "falling");
+    this.animationsMap.set("attack", "attack");
+    this.animationsMap.set("defeated", "dying");
   }
 
-  fall() {
-    this.anims.play("falling", true);
+  attackOnEnter() {
+    this.setVelocityX(0);
+    this.anims.play(this.animationsMap.get("attack"));
+    this.attackHitbox.body.x = !this.flipX
+      ? this.body.x + this.width * 0.4
+      : this.body.x - this.width * 0.4;
+    this.attackHitbox.body.y = this.body.y;
+    this.scene.physics.world.enableBody(this.attackHitbox);
+    //this.once("animationcomplete", () => this.stateMachine.setState("idle"));
   }
-
-  idle() {
-    super.idle("idle");
-  }
-
-  // Por definir
-  block() {}
-
-  moveLeft() {
-    super.moveLeft("walking");
-  }
-
-  moveRight() {
-    super.moveRight("walking");
-  }
-
-  jump() {
-    super.jump("jump");
-  }
-
-  fall() {
-    super.fall("falling");
-  }
-
-  // Por definir
-  specialAttack() {}
 }
--- src/scenes/BootLoader.js
+++ src/scenes/BootLoader.js
@@ -68,20 +68,54 @@ class BootLoader extends Phaser.Scene {
       attack: KeyCodes.L,
     });
 
-    //let teclaa = this.input.keyboard.addKey("a");
-    //teclaa.
-
+    // Jugador 1
     this.player1 = new Knight(this, 200, 100, Player1Controls);
     this.add.existing(this.player1);
     this.player1.setCollideWorldBounds(true);
+
+    // Jugador 2
     this.player2 = new Ninja(this, 200, 100, Player2Controls);
     this.add.existing(this.player2);
     this.player2.setCollideWorldBounds(true);
+
+    // Colisiones
     this.physics.add.collider([this.player1, this.player2], this.wall_floor);
+    this.physics.add.overlap(this.player1.attackHitbox, this.player2, () => {
+      this.player2.enemy = this.player1;
+      console.log(this.player2.healthPoints);
+      this.player2.stateMachine.setState("hit");
+    });
+    this.physics.add.overlap(this.player2.attackHitbox, this.player1, () => {
+      this.player1.enemy = this.player2;
+      console.log(this.player1.healthPoints);
+      this.player1.stateMachine.setState("hit");
+    });
+
+    this.player1HP = this.add
+      .bitmapText(
+        100,
+        25,
+        "pixelFont",
+        "PLAYER1 HP " + this.player2.healthPoints,
+        16
+      )
+      .setFontSize(10);
+
+    this.player2HP = this.add
+      .bitmapText(
+        400,
+        25,
+        "pixelFont",
+        "PLAYER2 HP " + this.player1.healthPoints,
+        16
+      )
+      .setFontSize(10);
   }
-  update(time, delta) {
-    this.player1.update();
-    this.player2.update();
+  update(time, deltaTime) {
+    this.player1.update(deltaTime);
+    this.player2.update(deltaTime);
+    this.player1HP.setText("PLAYER1 HP " + this.player1.healthPoints);
+    this.player2HP.setText("PLAYER2 HP " + this.player2.healthPoints);
   }
 }
 
